68033d6c1158a3f7508ee34f5a16a7a1988c001c
AMD <michojekunle1@gmail.com>
2022-08-14 13:17:15 +0100

    Introduced mui

---
+++ src/Container.js
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import Header from './components/Header';
+// import InputForm from './components/InputForm';
+// import Navbar  from './components/NavBar';
+
+// const Container = () => {
+//   return (
+//     <div className='container'>
+//         <Header/>
+//         <InputForm/>
+//         <Navbar/>
+//     </div>
+//   )
+// }
+
+// export default Container
